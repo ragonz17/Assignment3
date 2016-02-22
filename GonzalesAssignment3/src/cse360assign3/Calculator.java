bee3ba4c5ff1fc3ac6e1e40abdb43ace346eafa5
Ryan <ragonz17@asu.edu>
2016-02-22 11:26:03 -0700

    First Commit of Assignment 3

---
+++ GonzalesAssignment3/src/cse360assign3/Calculator.java
@@ -0,0 +1,74 @@
+
+/** Calculator fulfills basic mathematic functions
+*
+* @author Ryan Gonzales
+* @version Feb 22, 2016.
+*/
+package cse360assign3;
+
+public class Calculator 
+{
+	/** int total is the total within the Calculator */
+	private int total;
+
+	/** Calculator constructor initializes total to 0
+	*/
+	public Calculator () 
+	{
+		total = 0;  // not needed - included for clarity
+	}
+	
+	/** Returns the total within calculator
+	*
+	* @return total: calculated total at any given time
+	*/
+	public int getTotal () 
+	{
+		return 0;
+	}
+	
+	/** Adds an inputted value to the total
+	*
+	* @param value value to be added to total
+	*/
+	public void add (int value) 
+	{
+		
+	}
+	
+	/** Subtracts an inputted value from the total
+	*
+	* @param value value to be subtracted from the total
+	*/
+	public void subtract (int value) 
+	{
+		
+	}
+	
+	/** Multiplies an inputted value by the total
+	*
+	* @param value value to be multiplied by total
+	*/
+	public void multiply (int value) 
+	{
+		
+	}
+	
+	/** Adds an inputted value to the total
+	*
+	* @param value value to be added to total
+	*/
+	public void divide (int value) 
+	{
+		
+	}
+	
+	/** Returns a string of history within Calculator
+	*
+	* @return String of history
+	*/
+	public String getHistory () 
+	{
+		return "";
+	}
+}
---
+++ GonzalesAssignment3/src/cse360assign3/CalculatorTest.java
@@ -0,0 +1,58 @@
+package cse360assign3;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest 
+{
+
+	@Test
+	public void testCalculator() 
+	{
+		fail("Not yet implemented");
+	}
+	@Test
+	public void testgetTotal() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testadd() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testsubtract() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testmultiply() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testdivide() 
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testdivideZero()
+	{
+		fail("Not yet implemented");
+	}
+	
+	@Test 
+	public void testgetHistory() 
+	{
+		fail("Not yet implemented");
+	}
+
+	
+}
